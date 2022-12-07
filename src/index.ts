621f77d3f4d377bf8333159ccc64b137033ef4f0
xmflsct <me@xmflsct.com>
2022-12-07 19:52:29 +0100

    Fix ts

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,11 @@ import { NativeModules, Platform } from 'react-native'
 
 const detectLanguage = async (
   original: string
-): Promise<{ detected: string }> => {
+): Promise<{ language: string; confidence: number }[]> => {
   switch (Platform.OS) {
     case 'ios':
-      return await NativeModules.RNLanguageDetectionModule.detection(original)
     case 'android':
-      return {detected: await NativeModules.RNLanguageDetectionModule.detection(original)}
+      return await NativeModules.RNLanguageDetectionModule.detection(original)
     default:
       console.error('Platform not supported')
       return Promise.reject()
